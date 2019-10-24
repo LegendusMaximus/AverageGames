3b2951bcd586d78b8dbf08a0a53a40c1411643b4
LegendusMaximus <tapsandsinkstheo@gmail.com>
2019-10-24 09:57:44 +0100

    working maths bot version

--- MathsBot-version0.1.py
+++ MathsBot-version0.1.py
@@ -4,6 +4,12 @@ randomq = random.randint(0,100)
 randomq2 = random.randint(0,100)
 operation = ["+", "/", "*", "-"]
 roperation = random.choice(operation)
-question = str(randomq)+roperation+str(randomq2)
+question = "Enter the answer to"+str(randomq)+"+"+str(randomq2)
+answer = randomq+randomq2
 print(question)
+user_answer = int(input())
+if user_answer == answer:
+	print("Correct!")
+else:
+	print("Wrong!")
 input()
