af41aa55e1eada7dd0120c9fa78f764083b3dd64
LegendusMaximus <tapsandsinkstheo@gmail.com>
2019-10-24 10:02:18 +0100

    second working version

--- MathsBot-version0.1.py
+++ MathsBot-version0.1.py
@@ -1,15 +1,20 @@
 input()
-import random
-randomq = random.randint(0,100)
-randomq2 = random.randint(0,100)
-operation = ["+", "/", "*", "-"]
-roperation = random.choice(operation)
-question = "Enter the answer to"+str(randomq)+"+"+str(randomq2)
-answer = randomq+randomq2
-print(question)
-user_answer = int(input())
-if user_answer == answer:
-	print("Correct!")
-else:
-	print("Wrong!")
+points = 0
+import random	
+while True:
+	randomq = random.randint(0,100)
+	randomq2 = random.randint(0,100)
+	operation = ["+", "/", "*", "-"]
+	roperation = random.choice(operation)
+	question = "Enter the answer to"+str(randomq)+"+"+str(randomq2)
+	answer = randomq+randomq2
+	print(question)
+	user_answer = int(input())
+	if user_answer == answer:
+		print("Correct!")
+		points += 0.108
+	else:
+		print("Wrong!")
+		points = points-100
+	print("You have", points, "points!")
 input()
