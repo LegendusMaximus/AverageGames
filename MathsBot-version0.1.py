a61bc1baa448fdb3c722ba6e3c2376c46469facc
LegendusMaximus <tapsandsinkstheo@gmail.com>
2019-10-26 10:45:21 +0100

    fixed possible crash

--- MathsBot-version0.1.py
+++ MathsBot-version0.1.py
@@ -1,4 +1,5 @@
 input()
+user_answer = ""
 points = 0
 import random	
 while True:
@@ -9,10 +10,20 @@ while True:
 	question = "Enter the answer to"+str(randomq)+"+"+str(randomq2)
 	answer = randomq+randomq2
 	print(question)
-	user_answer = int(input())
+	while True:
+		try:
+			user_answer = int(input())
+			break
+		except:
+			points = points-200
+			print("Wrong! This is not a number")
+			print("You have", points, "points.")
+			print(question)
 	if user_answer == answer:
 		print("Correct!")
 		points += 0.108
+	elif user_answer == "q":
+		break
 	else:
 		print("Wrong!")
 		points = points-100
